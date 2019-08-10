bc154b978ee0d14f8dd88f66110958cfd01603f5
Chamnan Longlum <chamnan@irainnovation.com>
2019-08-10 11:10:36 +0700

    first

---
+++ site-config.js
@@ -0,0 +1,102 @@
+module.exports = {
+  // App Info
+  appstore_link         : null,                                  // Enter App Store URL.
+  playstore_link        : null,                                  // Enter Google Play Store URL.
+  google_analytics_ID   : "",                                        // Enter Google Analytics ID or ""
+  presskit_download_link: null,                                  // Enter a link to downloadable file or (e.g. public Dropbox link to a .zip file). 
+  video_or_screenshot   : "",                                           // "screenshot" or "video"
+  app_url               : "",                            // Domain of your website without path_prefix.
+  path_prefix           : "/",                                                    // Prefixes all links. For cases when deployed to example.github.io/automatic-gatsbyjs-app-landing-page/.
+  app_name              : "Fast Track",
+  app_price             : "",
+  app_description       : "Set Me Free",
+  app_keywords          : ["Fast Track", "School ", "Document", "Generate"],
+
+  // Personal Info
+  your_name              : "IRA",
+  your_link              : "",
+  your_city              : "",
+  email_address          : "contact@irainnovation.com",
+  linkedin_username      : null,
+  facebook_username      : null,
+  instagram_username     : "",
+  twitter_username       : "",
+  github_username        : "",
+  youtube_username       : null,
+
+  // Features List
+  features               : [
+    {
+      title: "FAST",
+      description:
+        "",
+      fontawesome_icon_name: "magic",
+    },
+    {
+      title: "Can Track Any",
+      description:
+        ".",
+      fontawesome_icon_name: "mobile",
+    },
+    {
+      title: "Notification alyway",
+      description:
+        "",
+      fontawesome_icon_name: "play-circle",
+    },
+    // {
+    //   title: "Optimized Performance",
+    //   description:
+    //     "No need to worry about page performance or optimizing images and videos. Everything is already configured for the best performance possible!",
+    //   fontawesome_icon_name: "sync",
+    // },
+    // {
+    //   title: "Easy to Tweak",
+    //   description:
+    //     "Tweak accent color, images, icons and transparency via the site-config.js file. No HTML/CSS needed.",
+    //   fontawesome_icon_name: "adjust",
+    // },
+    // {
+    //   title: "Feature List",
+    //   description:
+    //     "Add features (like this one) to your site via the site-config.js file. No HTML/CSS needed.",
+    //   fontawesome_icon_name: "star",
+    // },
+    // {
+    //   title: "Deploy to Netlify",
+    //   description: "Easily deploy your GatsbyJS landing page to Netlify.",
+    //   fontawesome_icon_name: "arrow-alt-circle-down",
+    // },
+    // {
+    //   title: "Social Links",
+    //   description:
+    //     "Easily add social media accounts and contact info in the footer via the site-config.js file. No HTML/CSS needed.",
+    //   fontawesome_icon_name: "link",
+    // },
+    // {
+    //   title: "FontAwesome Support",
+    //   description:
+    //     "Pick custom Font Awesome icons for the feature list via the site-config.js file. No HTML/CSS needed.",
+    //   fontawesome_icon_name: "info-circle",
+    // },
+  ],
+  header_background             : "#40E0D0",
+  topbar_title_color            : "#ffffff",
+  cover_overlay_color_rgba      : "#AFEEEE",
+  device_color                  : "black",                          // Set to "black", "blue", "coral", "white", or "yellow"
+  body_background_color         : "ffffff",
+  primary_text_color            : "#000",
+  content_width                 : "1170px",
+  font                          : `"kanit", sans-serif`,
+  link_color                    : "#1d63ea",
+  app_title_color               : "#ffffff",
+  app_price_color               : "#ffffff",
+  app_description_color         : "#ffffff",
+  feature_title_color           : "#000000",
+  feature_text_color            : "#666666",
+  feature_icons_foreground_color: "#1d63ea",
+  feature_icons_background_color: "#e6e6e6",
+  social_icons_foreground_color : "#666666",
+  social_icons_background_color : "#e6e6e6",
+  footer_text_color             : "#666666",
+}
